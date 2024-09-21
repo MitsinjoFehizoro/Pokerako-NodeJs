6e3e097efe78e05cf1dac83726ad953f831aad2e
MitsinjoFehizoro <mitsinjofehizoro@gmail.com>
2024-09-21 16:55:15 +0300

    Optimisation de la fonction auth()

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -2,67 +2,54 @@ import { NextFunction, Request, Response } from "express"
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import User from '../models/user-model';
-import { handleError } from "../tools/handle-error";
 dotenv.config()
 
-export const auth = (req: Request, res: Response, next: NextFunction) => {
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+
     const authorizationHeader = req.headers.authorization
-
-    if (authorizationHeader) {
-        const accessToken = authorizationHeader.split(' ')[1]
-        jwt.verify(
-            accessToken,
-            process.env.JWT_KEY as string,
-            (error, decoded) => {
-                if (error) {
-                    console.log('refresh token**********');
-
-                    const refreshToken = req.headers.cookie?.split('=')[1]
-                    jwt.verify(
-                        refreshToken as string,
-                        process.env.REFRESH_JWT_KEY as string,
-                        async (error, decoded) => {
-                            if (error)
-                                return res.status(403).json({
-                                    message: "🔒 refreshToken a expiré "
-                                })
-
-                            const user = await User.findByPk((decoded as { userId: string }).userId);
-
-                            if (!user)
-                                return res.status(404).json({
-                                    message: "🔒 Votre session a expiré "
-                                })
-
-                            const payload = { userId: user.id }
-                            const newAccessToken = jwt.sign(payload, process.env.JWT_KEY as string, { expiresIn: '15m' })
-
-                            res.setHeader(`Authorization`, `Bearer ${newAccessToken}`)
-                            req.headers.authorization = `Bearer ${newAccessToken}`
-
-                            jwt.verify(
-                                newAccessToken,
-                                process.env.JWT_KEY as string,
-                                (error, decoded) => {
-                                    if (error)
-                                        return res.status(403).json({
-                                            message: "🔒 Votre session a expiré "
-                                        })
-                                    req.body.user = decoded
-                                    next()
-                                }
-                            )
-                        }
-                    )
-                } else {
-                    req.body.user = decoded
-                    next()
-                }
-            }
-        )
-    } else {
-        res.status(401).json({
+    if (!authorizationHeader)
+        return res.status(401).json({
             message: "🔒 Jeton d'authentification requis. Veuillez vous authentifier."
         })
+
+    try {
+        const accessToken = authorizationHeader.split(' ')[1]
+        const decoded = jwt.verify(
+            accessToken,
+            process.env.JWT_KEY as string
+        )
+        req.body.user = decoded
+        next()
+    } catch (error) {
+        console.log('***** REFRESH TOKEN *****');
+        const refreshToken = req.headers.cookie?.split('=')[1]
+        const decoded = jwt.verify(
+            refreshToken as string,
+            process.env.REFRESH_JWT_KEY as string
+        )
+        const user = await User.findByPk((decoded as { userId: string }).userId)
+        if (!user) {
+            return res.status(403).json({
+                message: "🔒 Votre session a expiré. Veuillez se reconnecter."
+            })
+        }
+        const payload = { userId: user.id }
+        const newAccessToken = jwt.sign(payload, process.env.JWT_KEY as string, { expiresIn: '1m' })
+
+        res.setHeader(`Authorization`, `Bearer ${newAccessToken}`)
+        req.headers.authorization = `Bearer ${newAccessToken}`
+
+        jwt.verify(
+            newAccessToken,
+            process.env.JWT_KEY as string,
+            (error, decoded) => {
+                if (error)
+                    return res.status(403).json({
+                        message: "🔒 Votre session a expiré "
+                    })
+                req.body.user = decoded
+                next()
+            }
+        )
     }
 }
